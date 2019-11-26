db38a72e21b75ead1bce7c68347eb26066d985b8
shenshijun <ssj970763342@163.com>
2019-11-26 16:45:03 +0800

    test

--- githubconfigserver/src/main/java/org/ssj/GithubConfigServerApplication.java
+++ githubconfigserver/src/main/java/org/ssj/GithubConfigServerApplication.java
@@ -2,7 +2,9 @@ package org.ssj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class GithubConfigServerApplication {
     public static void main(String[] args) {
